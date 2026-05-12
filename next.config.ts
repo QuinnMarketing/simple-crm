de14b965469161de7856cdf3fe3e82e113836af2
analyticsquinnmarketing-code <analyticsquinnmarketing@gmail.com>
2026-05-12 20:45:07 +1000

    Include pdfkit AFM font data files in Vercel bundle
    Vercel doesn't auto-bundle pdfkit's font metrics files. Adding
    outputFileTracingIncludes to include node_modules/pdfkit/js/data/**

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
+  experimental: {
+    outputFileTracingIncludes: {
+      '**': ['./node_modules/pdfkit/js/data/**/*'],
+    },
+  },
 }
 
 export default nextConfig
